23704faa75dfd4ff2e78eb12177e4d6b0868f3a5
Analogic <33377537+KontenaIndonesia@users.noreply.github.com>
2020-03-14 23:45:48 +0700

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -3,14 +3,14 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Gatsby Ghost Casper`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Gastby with Ghost CMS and Casper Theme`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Marketing Metric Nerd`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `A site of marketing metric and analytics collection. Help marketer to make a plan for the next stage of each business challenges.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `metricXL.com`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
-    themeColor: `#15171A`, // Used for Offline Manifest
+    themeColor: `#FFF`, // Used for Offline Manifest
 }
