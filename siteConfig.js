c9a8848c98f4c2f05b39659d8a8d7890cf3c90b5
Analogic <33377537+KontenaIndonesia@users.noreply.github.com>
2020-03-15 00:08:04 +0700

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -3,8 +3,8 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `metricXL.com`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A site of marketing metric and analytics collection. Help marketer to make a plan for the next stage of each business challenges.`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Metric and Stats Nerd`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Collect marketing metric, stats, and analytics. Help marketer to make a plan for the next stage of each business challenges.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
